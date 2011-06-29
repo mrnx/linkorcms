91fdfb33de1d38b88c7384cba0b9339b22c8d268
Gennadiy <mmtel@pochta.ru>
2011-06-29 20:54:06 +0400

    Возвращаю случайно удаленные (из-за нестыковок версий) файлы, но admin.php БЕЗ Ваших последних правок, сорри, :-((

---
+++ admin.php
@@ -0,0 +1,87 @@
+<?php
+
+# LinkorCMS
+# © 2006-2011 Галицкий Александр Николаевич (linkorcms@yandex.ru)
+# Файл: admin.php
+# Назначение: Главная страница АДМИН-панели
+
+define('ADMIN_SCRIPT', true);
+define('VALID_RUN', true);
+
+require 'config/init.php'; // Конфигурация и инициализация
+define('ADMIN_FILE', System::config('admin_file')); // Ссылка на админ-панель
+
+// Проверка пользователя
+if(!($userAuth === 1 && $userAccess === 1 && System::user()->AllowCookie('admin', true))){
+	if(isset($_POST['admin_login'])){ // Проверка логина-пароля
+		$admin_name = SafeEnv($_POST['admin_name'], 255, str);
+		$admin_password = SafeEnv($_POST['admin_password'], 255, str);
+		$a = System::user()->Login($admin_name, $admin_password, false, true);
+		if($a === true && System::user()->SecondLoginAdmin){
+			System::user()->SetAdminCookie($admin_name, $admin_password);
+		}else{
+			System::user()->UnsetCookie('admin');
+			System::admin()->Login('Неверный логин или пароль'); // exit
+		}
+	}else{ // Форма авторизации
+		System::admin()->Login(); // exit
+	}
+}
+
+// Проверка, присутствует ли setup.php на сервере
+if(is_file('setup.php') && !is_file('dev.php')){
+	exit('<html>'."\n".'<head>'."\n".'	<title>'.CMS_NAME.' - !!!Ошибка!!!</title>'."\n".'</head>'."\n".'<body>'."\n".'	<center><h2>Удалите setup.php с сервера.</h2>
+		<br />
+		Админ-панель заблокирована.
+		<br />
+		Присутствие <b>setup.php</b> на сервере делает сайт<br />
+		уязвимым, поэтому, перед тем как начать работу,<br />
+		рекомендуется его <strong>удалить</strong>.</center>'."\n".'</body>'."\n".'</html>');
+}
+
+System::admin()->InitPage();
+
+// Получаем имя модуля
+$ModuleName = '';
+if(!isset($_GET['exe'])){
+	define('INDEX_PHP', true); // модуль на главной странице
+	$ModuleName = 'adminpanel';
+}else{
+	define('INDEX_PHP', false);
+	$ModuleName = SafeEnv($_GET['exe'], 255, str);
+	if($ModuleName == 'exit'){ // Выход
+		$user->UnsetCookie('admin');
+		GO(Ufu('index.php')); // exit
+	}
+}
+System::database()->Select('modules', "`enabled`='1' and `folder`='$ModuleName'");
+
+// Установлен такой модуль?
+if(System::database()->NumRows() == 0){
+	System::admin()->AddAdminMenu();
+	System::admin()->AddTextBox('Админ-панель - модуль не найден', '<div style="text-align: center;">Модуль "'.$ModuleName.'" не найден!</div>');
+	System::admin()->TEcho();
+	exit;
+}
+
+// Проверка на доступ
+if(!System::user()->CheckAccess2($ModuleName, $ModuleName)){
+	System::admin()->AddTextBox('Ошибка', $ModuleName.' Доступ закрыт!');
+	System::admin()->TEcho();
+	exit;
+}
+
+// Вспомогательные константы
+define('MOD_DIR', System::config('mod_dir').$ModuleName.'/');
+define('MOD_FILE', MOD_DIR.'admin.php');
+
+// Подключаем модуль
+if(is_file(MOD_FILE)){
+	System::admin()->AddAdminMenu();
+	require MOD_FILE;
+}
+
+// Вывод данных
+System::admin()->TEcho();
+
+?>
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * LinkorCMS 1.4
+ * © 2011 Александр Галицкий (linkorcms@yandex.ru)
+ * и LinkorCMS Development Group
+ */
+
+define('MAIN_SCRIPT', true);
+define('VALID_RUN', true);
+
+require 'config/init.php'; // Конфигурация и инициализация
+
+// ЧПУ
+if($config['general']['ufu'] && isset($_GET['ufu'])){
+	$_GET = UfuRewrite($_GET['ufu']);
+}
+
+// Закрыть сайт для пользователей
+if($config['general']['private_site'] && $user->AccessLevel() != 1){
+	include_once($config['inc_dir'].'template.login.php');
+	AdminShowLogin('Сайт закрыт для пользователей');
+}
+
+// Получаем имя модуля
+$ModuleName = '';
+if(!isset($_GET['name'])){
+	define('INDEX_PHP', true); // модуль на главной странице
+	$ModuleName = SafeEnv($config['general']['site_module'], 255, str, false, false);
+}else{
+	define('INDEX_PHP', false);
+	$ModuleName = SafeEnv($_GET['name'], 255, str);
+}
+$db->Select('modules', "`enabled`='1' and `folder`='$ModuleName'"); // Проверяем доступен ли данный модуль
+
+// Установлен такой модуль?
+if($db->NumRows() == 0){
+	System::site()->InitPage();
+	System::site()->AddTextBox('Ошибка', '<center>Данная страница ('.SafeDB($ModuleName, 255, str).') не существует или не доступна в данный момент.</center>');
+	System::site()->TEcho();
+	exit;
+}
+
+// Проверка на доступ
+$mod = $db->FetchRow();
+if(!$user->AccessIsResolved($mod['view'], $userAccess)){
+	System::site()->InitPage();
+	System::site()->AddTextBox('Ошибка', '<center>Доступ запрещен.</center>');
+	System::site()->TEcho();
+	exit;
+}
+
+// Вспомогательные константы
+define('MOD_DIR', $config['mod_dir'].$ModuleName.'/');
+define('MOD_FILE', MOD_DIR.'index.php');
+define('MOD_INIT', MOD_DIR.'init.php');
+define('MOD_THEME', RealPath2(SafeDB($mod['theme'], 255, str)));
+
+// Инициализация модуля
+$valid_init = file_exists(MOD_INIT);
+if($valid_init){
+	include MOD_INIT;
+	if(function_exists('mod_initialization')){
+		mod_initialization();
+	}
+}
+
+// Шаблонизатор
+if(!$system['no_templates']){
+	System::site()->InitPage();
+	$initfile = System::site()->Root.'init.php';
+	if(file_exists($initfile)){
+		include $initfile;
+	}
+}
+
+// Сообщения
+if(!$system['no_messages']){
+	include_once(System::config('inc_dir').'messages.inc.php');
+}
+
+// Подключаем модуль
+require MOD_FILE;
+
+// Сообщения внизу
+if(!$system['no_messages']){
+	BottomMessages();
+}
+
+// Вывод данных пользователю
+if(!$system['no_echo']){
+	System::site()->TEcho();
+}
+
+// Финализация модуля
+if($valid_init){
+	if(function_exists('mod_finalization')){
+		mod_finalization();
+	}
+}
+
+?>
